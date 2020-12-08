edf92ccfa066709cea1b9171b679e086efb32e9c
yoannlgl <55587947+yoann1605@users.noreply.github.com>
2020-12-08 22:37:56 +0100

    Update Deplacement.cs

--- Deplacement.cs
+++ Deplacement.cs
@@ -1,86 +1,38 @@
-// using System;
-// namespace Dames
-// {
-//     class Deplacement
-//     {
-//         //choix du pion
-//         public Pion choixBouger(List<Pion> possible)
-// 		{
-// 			//Si on peut bouger 
-// 			if (possible.Count > 0)
-// 			{
-// 				int xchoix = 0;
-// 				int count = 0;
-// 				int countPos = 1;
-// 				bool sxNumber = false;
+using System;
+namespace Dames
+{
+    class Deplacement
+    {
+        private string ligne(){
+         Console.WriteLine("Entrer le numero de ligne");
+         string response = Console.ReadLine();
+         return response;
+        }
 
-// 				String choix = "   ";
-// 				String sx = "";
-// 				bool bon = false;
-// 				count = possible.Count;
-// 				while (!numSeul.Match(choix).Success || !bon || !sxNumber)
-// 				{
-// 					Console.WriteLine("Choisiez Ou Aller");
-// 					for (int i = 0; i < possible.Count; i++)
-// 					{
+        private string colonne(){
+         Console.WriteLine("Entrer le numero de colonne");
+         string response = Console.ReadLine();
+         return response;
+        }
 
-// 						Console.WriteLine(countPos + "  La position :" + " [" + possible[i].Pos.x + "] " + "[" + possible[i].Pos.y + " ]");
+        private void checkCase(Case case, Pion pion, string couleur){
+            if(case.Occuper().Equals(true)){
+                if(pion.Couleur().Equals(couleur)){
+                    // f
+                }else{
+                    Console.WriteLine("Vous ne pouvez pas selectionner ce pion");
+                    this.ligne();
+                    this.colonne();
+                }
+            }else{
+               Console.WriteLine("La case est vide");
+            }
+        }
 
-// 						countPos = i + 1;
-// 					}
-// 					choix = Console.ReadLine();
 
+        private bool choix(int positionInit int positionDest){
+            if(){}
+        }
 
-// 					sx = choix[0].ToString();
-
-// 					sxNumber = int.TryParse(sx, out xchoix);
-// 					if (sxNumber)
-// 						xchoix = int.Parse(sx);
-
-// 					if (xchoix == 1)
-// 					{
-// 						bon = true;
-//                         return possible[0];
-// 					}
-
-// 					if (xchoix == 2)
-// 					{
-// 						if (count > 1)
-// 						{
-// 							if (possible[1].type == Case.rien)
-// 							{
-// 								bon = true;
-// 								return possible[1];
-// 							}
-// 						}
-// 					}
-
-// 					if (xchoix == 3)
-// 					{
-// 						if (count > 2)
-// 						{
-// 							if (possible[2].type == Case.rien)
-// 							{
-// 								bon = true;
-// 								return possible[2];
-// 							}
-// 						}
-// 					}
-
-// 					if (xchoix == 4)
-// 					{
-// 						if (count > 3)
-// 						{
-// 							if (possible[3].type == Case.rien)
-// 							{
-// 								bon = true;
-// 								return possible[3];
-// 							}
-// 						}
-// 					}
-// 				}
-// 			}
-// 			return new Pion();
-// 		}
-//     }
-// }
+    }
+}
